e86193b81f85833b91421054b03ee8ef2920e4cb
Sythmaster <Sythmaster@gmail.com>
2011-03-10 18:11:23 -0700

    now builds a gateway and zealots.  zealots just stand there.  was having some "array bounds" issues, not sure if thats fixed.

--- scbwai/ProxyBot-2.6.1/ProxyBot/starcraftbot/proxybot/bot/ExampleStarCraftBot.java
+++ scbwai/ProxyBot-2.6.1/ProxyBot/starcraftbot/proxybot/bot/ExampleStarCraftBot.java
@@ -21,6 +21,13 @@ public class ExampleStarCraftBot implements StarCraftBot {
 
 	/** specifies that the agent is running */
 	boolean running = true;
+	
+	boolean powered[][] = null;
+	boolean gatewayBuild = false;
+	boolean gatewayBuilding = false;
+	boolean pylonBuilding = false;
+	
+	private Game g;
 
 	public JPanel getPanel() {
 		return null;
@@ -33,6 +40,8 @@ public class ExampleStarCraftBot implements StarCraftBot {
 	 */
 	public void start(Game game) {
 
+		g = game;
+		
 		// run until told to exit
 		while (running) {
 			try {
@@ -43,6 +52,21 @@ public class ExampleStarCraftBot implements StarCraftBot {
 			// start mining
 			System.out.println("NEW TURN:::::::::");
 			
+			powered = new boolean[game.getMap().getMapHeight()][game.getMap().getMapWidth()];
+			
+			game.getCommandQueue().setColor(1);
+			
+			for(int i = 0; i < game.getMap().getMapHeight();i++)
+			{
+				//powered[i] = new [];
+				for(int j = 0; j < game.getMap().getMapWidth();j++)
+				{
+					powered[i][j] = false;
+				}
+			}
+			
+			
+			
 			UnitWME Nexus = null;
 			for(UnitWME unit : game.getPlayerUnits()){
 				if(unit.getIsCenter())
@@ -50,18 +74,71 @@ public class ExampleStarCraftBot implements StarCraftBot {
 					Nexus = new UnitWME(unit);
 					break;
 				}
+				else if(unit.getTypeID() == UnitTypeWME.Protoss_Gateway)
+				{
+					if(unit.getIsBuilding())
+					{
+						gatewayBuilding = true;
+					}
+					if(unit.getIsBuilt())
+					{
+						gatewayBuild = true;
+						gatewayBuilding = false;
+					}
+				}
+				else if(unit.getIsFarm())
+				{
+					if(unit.getIsBuilding())
+					{
+						pylonBuilding = true;
+					}
+					else
+					{
+						powered[unit.getRealY()][unit.getRealX()] = true;
+						//mark all areas w/ circle realX,realY radius... 10? as powered....
+						
+						int radius = 10;
+						int X = unit.getRealX();
+						int Y = unit.getRealY();
+						//fit circular peg (psy matrix) into square hole (map)
+						//
+						//   (x-a)^2 + (y-b)^2 = r^2  circle @ (a,b) w/ r radius
+						//
+						
+						int currentX = X - radius - 1;
+						int currentY, maxY;
+						while(currentX < X+radius && currentX < game.getMap().getMapWidth())
+						{
+							currentY = (int)Math.sqrt((double) Math.pow(radius, 2.0) - Math.pow(X-currentX, 2.0));
+							maxY = Y + Math.abs(Y-currentY);
+							if(currentY > maxY)
+							{
+								int t = maxY;
+								maxY = currentY;
+								currentY = t;
+							}
+							System.out.println("for curX["+currentX+"], lowY:"+currentY+" & maxY:"+maxY);
+							while(currentY < maxY  && currentY < game.getMap().getMapHeight())
+							{
+								powered[currentY][currentX] = true;
+								currentY++;
+							}
+							currentX++;
+							
+						}
+						
+						
+						game.getCommandQueue().drawCircleScreen(unit.getRealX(), unit.getRealY(), 10, true);
+						game.getCommandQueue().drawCircleMap(unit.getRealX(), unit.getRealY(), 10, true);
+						game.getCommandQueue().drawCircleMap(unit.getX(), unit.getY(), 10, true);
+						game.getCommandQueue().drawCircleScreen(unit.getX(), unit.getY(), 10, true);
+					}
+				}
 			}
 			
 			for (UnitWME unit : game.getPlayerUnits()) {
 				if (unit.getIsWorker())
 				{
-					
-					//System.out.print("At unit worker, current order is:");
-					
-					//String OrderName = Order.;
-
-					
-					
 					//System.out.println(unit.getOrder()+" which is :"+Order.values()[unit.getOrder()].toString());
 					if(!((unit.getOrder() == Order.MiningMinerals.ordinal())
 						|| (unit.getOrder() == Order.ReturnMinerals.ordinal())
@@ -71,15 +148,12 @@ public class ExampleStarCraftBot implements StarCraftBot {
 						|| (unit.getOrder() == Order.Harvest2.ordinal())
 						|| (unit.getOrder() == Order.Harvest3.ordinal())
 						|| (unit.getOrder() == Order.Harvest4.ordinal())
-						|| (unit.getOrder() == Order.Harvest5.ordinal())
-						|| (unit.getOrder() == Order.Move.ordinal()))
-						/*|| !(unit.getOrder() == Order.Build5.ordinal())
-						|| !(unit.getOrder() == Order.BuildingLand.ordinal())
-						|| unit.getOrder() == Order.PlayerGuard.ordinal()*/) {
+						|| (unit.getOrder() == Order.Harvest5.ordinal()))) 
+					{
 
 					// System.out.println("Assigning worker of type:"+
 					// unit.getType().getName()+": to mine...");
-					System.out.println("Worker unit["+unit.getID()+"] was doing:"+Order.values()[unit.getOrder()].toString()+" -- setting it to Mine now.");
+					//System.out.println("Worker unit["+unit.getID()+"] was doing:"+Order.values()[unit.getOrder()].toString()+" -- setting it to Mine now.");
 					int patchID = -1;
 					double closest = Double.MAX_VALUE;
 
@@ -128,59 +202,100 @@ public class ExampleStarCraftBot implements StarCraftBot {
 				game.getCommandQueue().train(Nexus.getID(),workerType);
 							//center = new UnitWME(unit);
 			}
-
+			//make zealots
+			if(game.getPlayer().getMinerals() >= 100 && gatewayBuild)
+			{
+				for(UnitWME u : game.getPlayerUnits())
+				{
+					if(u.getTypeID() == UnitTypeWME.Protoss_Gateway)
+					{
+						game.getCommandQueue().train(u.getID(), UnitTypeWME.Protoss_Zealot);
+					}
+				}
+			}
+			
+			//if we have pylons, build a gateway
+			if(game.getPlayer().getMinerals() >= 150 && !gatewayBuild && !gatewayBuilding)
+			{
+				UnitWME pylon = null;
+				for(UnitWME u : game.getPlayerUnits())
+				{
+					if(u.getIsFarm())
+					{
+						pylon = u;
+						break;
+					}
+				}
+				int RandomX,RandomY;
+				if(pylon != null)
+				{
+					RandomX = (int) (pylon.getRealX() + (Math.random()*9));
+					RandomY = (int) (pylon.getRealY() + (Math.random()*9));
+					
+					boolean flip = false;
+					while(!game.getMap().isBuildable(RandomX, RandomY, 4, 3) && hasPower(RandomX, RandomY, 4, 3))
+					{
+						if(flip)
+						{
+							RandomX++;
+							flip = false;
+						}
+						else
+						{
+							RandomY++;
+							flip = true;
+						}
+						if(RandomX > pylon.getRealX()+9 || RandomX > game.getMap().getMapWidth()-5) //bounds case, building is width of 4...
+						{
+							RandomX = pylon.getRealX()-10;
+							if(RandomX < 0)
+								RandomX = 0;
+						}
+						if(RandomY > pylon.getRealY()+9 || RandomY > game.getMap().getMapHeight()-4) //bounds case, building is height of 3...
+						{
+							RandomY = pylon.getRealY()-10;
+							if(RandomY < 0)
+								RandomY = 0;
+						}
+					}
+					for(UnitWME u : game.getPlayerUnits())
+					{
+						if(u.getIsWorker())
+						{
+							System.out.println("BUILDING A GATEWAY AT:: ("+RandomX+","+RandomY+") near pylon @("+pylon.getRealX()+","+pylon.getRealY()+")!!");
+							game.getCommandQueue().build(u.getID(), RandomX, RandomY, UnitTypeWME.Protoss_Gateway);
+							break;
+						}
+					}
+				}
+				
+			}
+			
 			// build more supply
 
 			int NonBuildingUnits = 0;
 			int PrevSupplyUnits = 0;
 			int supplyType = UnitTypeWME.getSupplyType(game.getPlayerRace());
 
-			for (UnitWME unit : game.getPlayerUnits()) {
-				if (!unit.getIsBuilding())
+			for (UnitWME unit : game.getPlayerUnits()) 
+			{
+				if (unit.getTypeID() == UnitTypeWME.Protoss_Probe || unit.getTypeID() == UnitTypeWME.Protoss_Zealot)
 					NonBuildingUnits++;
-				else if (unit.getIsFarm())
+				else if (unit.getTypeID() == UnitTypeWME.Protoss_Pylon)
 					PrevSupplyUnits++;
 			}
 
-			System.out.println("CURRENT # of Supply Units:"+NonBuildingUnits+": SUPPLY CAP IS: "+ 9 +(PrevSupplyUnits*8));
+			//System.out.println("CURRENT # of Supply Units:"+NonBuildingUnits+": SUPPLY CAP IS: "+ (9 +(PrevSupplyUnits*8)));
 			
 			if (game.getPlayer().getMinerals() >= 100
-					&& NonBuildingUnits > (9 + (PrevSupplyUnits * 8) - 2)
-			/*
-			 * game.getPlayer().getSupplyUsed() >=
-			 * (game.getPlayer().getSupplyTotal() - 2)
-			 */) {
-				//int supplyType = UnitTypeWME.getSupplyType(game.getPlayerRace());
-
-				//System.out.println("Hit the need to build another farm...");
-
+					&& NonBuildingUnits > (9 + (PrevSupplyUnits * 8) - 2)) {
 				// build a farm
-					int workerType = UnitTypeWME.getWorkerType(game
-							.getPlayerRace());
+					int workerType = UnitTypeWME.getWorkerType(game.getPlayerRace());
 					for (UnitWME unit : game.getPlayerUnits()) {
 						if (unit.getTypeID() == workerType) {
 							
-							
-
-							/*System.out.println("Assigning a unit to build supplyType:"
-											+ UnitTypeWME.getSupplyType(game
-													.getPlayerRace())
-											+ "| should be...["
-											+ UnitTypeWME.Protoss_Pylon + "]");
-*/
-							// pick a random spot near the worker
-							//int centerX=0,centerY=0;
+							//pick spot near Nexus
 							int buildX=0,buildY=0;
-							/*for(UnitWME u : game.getUnits())
-							{
-								if(u.getIsCenter())
-								{
-									centerX = u.getX();
-									centerY = u.getY();
-									break;
-								}
-							}*/
-							
 							buildX = Nexus.getRealX() + (int) (Math.random() * 5.0);
 							buildY = Nexus.getRealY() + (int) (Math.random() * 5.0);
 							if(buildX == Nexus.getRealX() && buildY == Nexus.getRealY())
@@ -200,8 +315,6 @@ public class ExampleStarCraftBot implements StarCraftBot {
 							}
 							System.out.print("checking spot ["+buildX+","+buildY+"] to see if buildable for Pylon by center["+Nexus.getRealX()+","+Nexus.getRealY()+"]...");
 							System.out.println("Range of map is {0.."+game.getMap().getMapWidth()+"} wide and {0.."+game.getMap().getMapHeight()+"} height");
-							//UnitWME pylon = new UnitWME();
-							
 							boolean flip = true;
 							while(!(game.getMap().isBuildable(buildX, buildY, 2, 2))) //pylon is 2x2 tiles
 							{
@@ -234,25 +347,33 @@ public class ExampleStarCraftBot implements StarCraftBot {
 															 buildY, //unit.getY() + (int) (-10.0 + Math.random() * 20.0),
 															 supplyType);
 								game.getCommandQueue().rightClick(unit.getID(), buildX, buildY);
-								//game.getCommandQueue().stop(unit.getID());
-								//game.getCommandQueue().drawCircleScreen(buildX, buildY, 1, true);
-								//game.getCommandQueue().drawCircleMap(buildX, buildY, 1, true);
 								game.getCommandQueue().drawRectScreen(buildX, buildY, buildX+1, buildY-1);
 								game.getCommandQueue().drawRectMap(buildX, buildY, buildX+1, buildY-1);
-								//game.getCommandQueue().drawBoxScreen(buildX, buildY, buildX+1, buildY+1);
-								
-								
-								//unit.setOrder(Order.Build5.ordinal());
-							//}
-							/*else{
-								System.out.println("CANT BUILD AT RANDOM SPOT!");
-							}*/
+
 							break;
 						}
 					}
 				}
 
 		}
+	}
+
+	private boolean hasPower(int randomX, int randomY, int i, int j) {
+		// TODO Auto-generated method stub
+		for(int a = 0; a < j; a++)
+		{
+			if(randomY+a > g.getMap().getMapHeight()-1)
+				return false;
+			for(int b = 0; b < i; b++)
+			{
+				if(randomX+b > g.getMap().getMapWidth()-1)
+					return false;
+				else if(!powered[randomY+a][randomX+b])
+					return false;
+			}
+		}
+		
+		return true;
 	}
 
 	/**
