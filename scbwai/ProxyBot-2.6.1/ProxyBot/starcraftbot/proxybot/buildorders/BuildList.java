e24d22864788c1d490b41b298615e31e3c219b03
tempostudios com <tempostudios.com@gmail.com>
2011-05-03 23:58:45 -0600

    BuildOrder didn't get sent last time.

---
+++ scbwai/ProxyBot-2.6.1/ProxyBot/starcraftbot/proxybot/buildorders/BuildList.java
@@ -0,0 +1,43 @@
+package starcraftbot.proxybot.buildorders;
+import java.util.*;
+
+
+
+public class BuildList {
+	private int supply;
+	private LinkedList<BuildOrder> orders = null;
+	private String versus = null;
+	public BuildList(){
+		supply = 0;
+		orders = new LinkedList<BuildOrder>();
+		versus = "default";
+	}
+	public BuildList(int n){
+		supply = n;
+		orders = new LinkedList<BuildOrder>();
+		versus = "default";
+	}
+	public BuildList(int n, String v){
+		supply = n;
+		orders = new LinkedList<BuildOrder>();
+		versus = v;
+	}
+	public void addOrder(BuildOrder order){
+		orders.add(order);
+	}
+	public String getVersus(){
+		return versus;
+	}
+	public int getSupply(){
+		return supply;
+	}
+	public int getLength(){
+		return orders.size();
+	}
+	public LinkedList<BuildOrder> getList(){
+		return orders;
+	}
+	public String toString(){
+		return "Order:"+orders.size();
+	}
+}
